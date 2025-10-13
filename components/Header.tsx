21e172520ee63cf3c125d2c151d60d218442cf3a
carlozzsilva10 <0332202039@unjfsc.edu.pe>
2025-10-12 21:49:50 -0500

    Se arregla el diseño del Navbar, que antes se despegaba del Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,14 +22,14 @@ const Header = () => {
     }, []);
 
     return (
-        <header className = {`fixed max-w-[1920px] w-full bg-primary-200 top-0 right-0 left-0 transition-all z-50 ${headerActive ? "h-[85px]" : "h-[110px]"}`}>
+        <header className = {`fixed max-w-[1920px] w-full bg-primary-200 top-0 right-0 left-0 transition-all z-50 overflow-visible ${headerActive ? "h-[85px]" : "h-[110px]"}`}>
             <div className = "container mx-auto h-full flex items-center justify-between">
                 {/* logo */}
                 <Link href = "">
                     <Image src = "/assets/img/logo.png" width = {117} height = {55} alt = "logo" />
                 </Link>
                 {/* mobile nav */}
-                <MobileNav containerStyles = {`flex xl:hidden flex-col text-center w-full left-0 text-base uppercase font-medium text-white gap-8 fixed bg-primary-200 transition-all ${headerActive ? "top-[100px]" : "top-[124px]"} ${openNav ? "max-h-max pt-8 pb-10 border-t border-white/10" : "max-h-0 pt-0 pb-0 overflow-hidden border-white/0"}`} />
+                <MobileNav containerStyles = {`flex xl:hidden flex-col text-center w-full left-0 text-base uppercase font-medium text-white gap-8 absolute inset-x-0 top-full bg-primary-200 transition-all ${openNav ? "max-h-max pt-8 pb-10 border-t border-white/10" : "max-h-0 pt-0 pb-0 overflow-hidden border-white/10"}`} />
                 {/* desktop nav */}
                 <Nav containerStyles = "py-12 gap-4 text-white text-base uppercase font-medium transition-all hidden xl:flex" />
                 {/* hide/open menu button */}
@@ -43,7 +43,7 @@ const Header = () => {
                             Registrarse
                         </button>
                     </div>
-                    <button className = "text-white xl:hidden" onClick = {() => setOpenNav(!openNav)}>
+                    <button className = "text-white xl:hidden" onClick= {() => setOpenNav(!openNav)}>
                         <MdMenu className = "text-4xl" />
                     </button>
                 </div>
